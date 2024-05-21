204f06692d7b141fbd7d91800666af1198af44cd
Luca Hendrik Helms <github@lucahelms.dev>
2024-05-21 23:21:02 +0200

    add playing field component (#2)

---
+++ 1/src/components/PlayingField.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import type { Color } from "@/types/Colors";
+
+interface Props {
+  fieldColor: Color;
+  text?: string;
+  textColor?: "white" | "black";
+}
+
+withDefaults(defineProps<Props>(), {
+  text: "",
+  textColor: "black",
+});
+</script>
+
+<template>
+  <div :class="['playingfield flex justify-center items-center', $props.fieldColor]">
+    <span :class="['font-bold field-color', $props.textColor]">{{ $props.text }}</span>
+  </div>
+</template>
+
+<style>
+.playingfield {
+  border-radius: 50%;
+  width: 3.125rem;
+  height: 3.125rem;
+  border: 0.188rem solid #000;
+
+  &.green {
+    @apply bg-green-700;
+  }
+
+  &.yellow {
+    @apply bg-amber-400;
+  }
+
+  &.black {
+    @apply bg-black;
+  }
+
+  &.red {
+    @apply bg-red-600;
+  }
+}
+
+.field-color {
+  &.white {
+    @apply text-white;
+  }
+
+  &.black {
+    @apply text-black;
+  }
+}
+</style>
