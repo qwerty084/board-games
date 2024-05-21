982e437b67168161a7504fcab90681f6a89ec418
Luca Hendrik Helms <github@lucahelms.dev>
2024-05-21 23:17:21 +0200

    add playing field component

--- 1/src/components/BaseElement.vue
+++ 1/src/components/BaseElement.vue
@@ -1,55 +1,28 @@
 <script setup lang="ts">
-type Color = "green" | "yellow" | "red" | "black";
+import type { Color } from "@/types/Colors";
+import PlayingField from "./PlayingField.vue";
 
-const props = defineProps<{
+defineProps<{
   color: Color;
 }>();
 </script>
 
 <template>
-  <div class="grid">
-    <div class="row">
-      <div :class="props.color"></div>
-      <div :class="props.color"></div>
+  <div class="grid h-fit">
+    <div class="flex flex-col gap-4">
+      <PlayingField :fieldColor="$props.color" />
+      <PlayingField :fieldColor="$props.color" />
     </div>
     <div class="flex h-full justify-center items-center mx-auto font-bold text-xl">B</div>
-    <div class="row">
-      <div :class="props.color"></div>
-      <div :class="props.color"></div>
+    <div class="flex flex-col gap-4">
+      <PlayingField :fieldColor="$props.color" />
+      <PlayingField :fieldColor="$props.color" />
     </div>
   </div>
 </template>
 
 <style scoped>
-.row div {
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  border: 3px solid #000;
-}
-
 .grid {
   grid-template-columns: 1fr 30px 1fr;
-  height: fit-content;
-}
-
-.row {
-  @apply flex flex-col gap-4;
-}
-
-.green {
-  background-color: #077706;
-}
-
-.yellow {
-  background-color: #e6e307;
-}
-
-.black {
-  background-color: #000;
-}
-
-.red {
-  background-color: #ea0505;
 }
 </style>
